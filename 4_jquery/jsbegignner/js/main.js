98c0b629ac88ef8a12e27d93c2e9d7e86b5d3c62
ryo ishiguro <ishiguroryo65@gmail.com>
2019-05-25 16:37:50 +0800

    beginner

---
+++ 4_jquery/jsbegignner/js/main.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// 1.----
+
+var myNumber = 2.5;                   // a number
+var myString = "This is a string ";     // a string
+var myBoolean = true;               // a boolean
+var myArray = ["Lucy","Panda","Grizzly","Hunter"];
+
+
+console.log(myNumber);
+console.log(myString);
+console.log(myBoolean);
+console.log(myArray);
+
+// 2.----
+
+var jojo = "hello,world!";
+console.log(jojo);
+
+var hamon = jojo.charAt(0);
+var stand = jojo.charAt(11);
+
+console.log(hamon);
+console.log(stand);
+
+var jojo = "Hello,world!";
+console.log(jojo);
+
+
+var count = jojo.length;
+console.log(count);
+
+//
+// var str = 文字列;
+//
+// str.charAt( 文字の位置 );
+
+
+// 3.----
+
+var num1 = 300;
+var num2 = 200;
+
+var answer = num1 + num2;
+var answer2 = num1 * num2;
+
+
+console.log(answer);
+console.log(answer2);
