272bae381af024aace9dbdffc46f8af11e0f6434
ryo ishiguro <ishiguroryo65@gmail.com>
2019-06-14 08:35:34 +0800

    jquery1

---
+++ 5_mysql/jquery1/js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$('h1').length
+<h1>My first jquery page</h1>
+
+$("h1").length
+<h1>My first jquery page</h1>
+
+$('firstParagraph')
+<p class="firstParagraph">this is a paragraqh.</p>
+
+$('#secondParagraph')
+<p id="firstParagraph">this is a paragraqh.</p>
+
+$('p').hide();
+
+$('p').show();
+
+$('p').remove();
+
+$('input').val('ABC');//文字が違うと上書きされる
