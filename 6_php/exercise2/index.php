1b75220b5926afc063acee49943da815d8140e4e
ryo ishiguro <ishiguroryo65@gmail.com>
2019-07-22 20:41:06 +0800

    fix3

--- 6_php/exercise2/index.php
+++ 6_php/exercise2/index.php
@@ -1,24 +1,18 @@
-<?php
+<?php echo  "---------------1-------------------" ;  echo "<br>\n";
 
   $Ryo = ["adobo","basketball","Hunter x Hunter","coldrain"];  echo "<br>\n";
 
-  print_r($Ryo);  echo "<br>\n";
+  print_r($Ryo);   echo "<br>\n";
 
   echo $Ryo[0];  echo "<br>\n";
 
   echo $Ryo[3];  echo "<br>\n";
 
-  $Ryo[3] = "Ryo"
-  print_r($Ryo);  echo "<br>\n";
+  $Ryo[3] = "Ryo";
+  print_r($Ryo);  echo "<br>\n"; echo "<br>\n";
 
+  echo  "---------------2-------------------" ;  echo "<br>\n";
 
- ?>
-
-
-
- <?php echo  "---------------2-------------------" ;  echo "<br>\n"; ?>
-
- <?php
 
    $countryscapital  = [
      "japan" => "tokyo",
@@ -30,4 +24,4 @@
 
    print_r($countryscapital);   echo "<br>\n";
 
-  ?>
+?>
