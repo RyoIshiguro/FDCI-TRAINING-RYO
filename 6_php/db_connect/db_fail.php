c111b49fa5ed8f3bf6b60fbfa2c03392dff85c86
ryo ishiguro <ishiguroryo65@gmail.com>
2019-09-04 08:39:20 +0800

    fix_db_connect2

--- 6_php/db_connect/db_fail.php
+++ 6_php/db_connect/db_fail.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <?php
-      echo "fail";
-     ?>
-  </body>
-</html>
+<?php
+  echo "fail";
+ ?>
--- 6_php/db_connect/db_success.php
+++ 6_php/db_connect/db_success.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <?php
-      echo "success";
-     ?>
-  </body>
-</html>
+<?php
+  echo "success";
+?>
